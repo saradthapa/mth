b46034929c983253803baf46da2b94858a7158fd
Sarad Thapa <saradthapa@gmail.com>
2019-05-18 17:12:25 +0000

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = grunt => {
 
 	require('load-grunt-tasks')(grunt);
 
-	let port = grunt.option('port') || 80;
+	let port = grunt.option('port') || 8080;
 	let root = grunt.option('root') || '.';
 
 	if (!Array.isArray(root)) root = [root];
